13d41bcf66df78b2e6acf52b803b80381729b122
Luis Carmona <lcarmona@nyc.yearup.org>
2019-12-06 14:10:41 -0500

    form input general layout

---
+++ myapp.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+
+get '/' do
+  # 'Hello world!'
+  # erb :home
+  erb :home, :layout => :index
+end
+
+get '/user/new' do
+  erb :newuser
+end
+
+get '/user/:name' do
+  # "Hello #{params['name']}!"
+  # puts params
+  # @school = 'Year Up'
+  erb :user, :layout => :index
+end
+
+post '/user' do
+
+  puts params
+
+  redirect to('/user/'+params['user_name'])
+end
+
+#{"user_name"=>"Luis", "user_mail"=>"luisc@xample.com", "user_fun"=>"Like koalas"}
+# set up a page with a form
+# send a post request with the form
+# the form should have name, email, and fun fact
+# the post request should redirect you to a page where the form input is rendered
